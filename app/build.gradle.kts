3fb86031c03a0e8b622637dfe5448c61d6002544
tunnellight392 <therualex@gmail.com>
2026-09-05 20:12:40 -0400

    Add Tou length and clock category

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.tunnellight.converter"
         minSdk = 29
         targetSdk = 37
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
